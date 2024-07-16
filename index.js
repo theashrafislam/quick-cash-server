13de009bbcb2b0e01e27abb292f0188602e29f35
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-07-16 16:16:54 +0600

    fixed register operation some bug

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.POST || 5000;
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 
 //middelware
 app.use(express.json());
@@ -15,57 +16,68 @@ const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PAS
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const registerUser = client.db("quick-cash").collection("registerUser");
-
-
+        const registerUser = client.db("quick-cash").collection("registerUser");
 
 
-    app.post("/register-user", async (req, res) => {
-        const info = req.body;
-        const query = {email: info.email}
-        const find = await registerUser.findOne(query);
-        if(!find){
-            const result = await registerUser.insertOne(info);
-            return res.send(result);
-        }
-        else{
-            return res.status(400).send({message: 'Email is already used.'})
-        }
-    })
-
-    app.get('/register-user', async(req, res) => {
-        const result = await registerUser.find().toArray();
-        res.send(result);
-    })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post("/register-user", async (req, res) => {
+            const info = req.body;
+            const query = {
+                $or: [
+                    { email: info.email },
+                    { mobileNumber: info.mobileNumber }
+                ]
+            };
+            const find = await registerUser.findOne(query);
+            if (!find) {
+                const hashedPin = await bcrypt.hash(info.pin, 10);
+                info.pin = hashedPin;
+                const result = await registerUser.insertOne(info);
+                return res.send(result);
+            }
+            else {
+                return res.status(400).send({ message: 'Email / Mobile Number is already used.' })
+            }
+        })
+
+        app.post('/login', async (req, res) => {
+            
+        })
+
+        app.get('/register-user', async (req, res) => {
+            const result = await registerUser.find().toArray();
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello, I am Quick Cash Server. Right Now I am all OKAY!')
+    res.send('Hello, I am Quick Cash Server. Right Now I am all OKAY!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
